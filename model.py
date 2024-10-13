d04790975698553c5a6a9c28b4523d485307fc58
Wenwen Min <minlab@aliyun.com>
2024-10-13 10:27:57 +0800

    Update model.py

--- model.py
+++ model.py
@@ -32,42 +32,6 @@ class FeedForward(nn.Module):
         return self.net(x)
 
 
-class Attention(nn.Module):
-    def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
-        super().__init__()
-        inner_dim = dim_head * heads  # 64*8 = 512
-        project_out = not (heads == 1 and dim_head == dim)
-        self.heads = heads
-        self.scale = dim_head ** -0.5
-        self.attend = nn.Softmax(dim=-1)
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
-        self.to_out = nn.Sequential(
-            nn.Linear(inner_dim, dim),
-            nn.Dropout(dropout)
-        ) if project_out else nn.Identity()
-
-    def forward(self, x):
-        b, n, _, h = *x.shape, self.heads
-        qkv = self.to_qkv(x).chunk(3, dim=-1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), qkv)
-        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
-        attn = self.attend(dots)
-        out = einsum('b h i j, b h j d -> b h i d', attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        return self.to_out(out)
-
-
-class attn_block(nn.Module):
-    def __init__(self, dim, heads, dim_head, mlp_dim, dropout=0.):
-        super().__init__()
-        self.attn = PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout))
-        self.ff = PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
-
-    def forward(self, x):
-        x = self.attn(x) + x
-        x = self.ff(x) + x
-        return x
-
 
 class ImageEncoder(nn.Module):
     def __init__(self):
@@ -168,87 +132,9 @@ class ProjectionHead(nn.Module):
         return x
 
 
-class mclSTExp_MLP(nn.Module):
-    def __init__(self, temperature, image_embedding, spot_embedding, projection_dim, dropout=0.):
-        super().__init__()
-        self.x_embed = nn.Embedding(65536, spot_embedding)
-        self.y_embed = nn.Embedding(65536, spot_embedding)
-        self.image_ecode = ImageEncoder()
-        self.image_projection = ProjectionHead(embedding_dim=image_embedding, projection_dim=projection_dim)
-        self.spot_projection = ProjectionHead(embedding_dim=spot_embedding, projection_dim=projection_dim)
-        self.temperature = temperature
-
-    def forward(self, batch):
-        image_features = self.image_ecode(batch["image"])
-        spot_features = batch["expression"]
-        image_embeddings = self.image_projection(image_features)
-        x = batch["position"][:, 0].long()
-        y = batch["position"][:, 1].long()
-        centers_x = self.x_embed(x)
-        centers_y = self.y_embed(y)
-
-        spot_features = spot_features + centers_x + centers_y
-
-        spot_embeddings = self.spot_projection(spot_features)
-        cos_smi = (spot_embeddings @ image_embeddings.T) / self.temperature
-        label = torch.eye(cos_smi.shape[0], cos_smi.shape[1]).cuda()
-        spots_loss = F.cross_entropy(cos_smi, label)
-        images_loss = F.cross_entropy(cos_smi.T, label.T)
-        loss = (images_loss + spots_loss) / 2.0  # shape: (batch_size)
-        return loss.mean()
-
-
-class mclSTExp_Attention(nn.Module):
-    def __init__(self,encoder_name, temperature, image_dim, spot_dim, projection_dim, heads_num, heads_dim, head_layers, dropout=0.):
-        super().__init__()
-        self.x_embed = nn.Embedding(65536, spot_dim)
-        self.y_embed = nn.Embedding(65536, spot_dim)
-        if encoder_name == "resnet50":
-            self.image_encoder = ImageEncoder_Resnet()
-        if encoder_name == "densenet121":
-            self.image_encoder = ImageEncoder()
-        if encoder_name == "vit":
-            self.image_encoder = ImageEncoder_VIT()
-        if encoder_name == "res18":
-            self.image_encoder = ImageEncdoer_res18()
-        if encoder_name == "res101":
-            self.image_encoder = ImageEncdoer_res101()
-        self.spot_encoder = nn.Sequential(
-            *[attn_block(spot_dim, heads=heads_num, dim_head=heads_dim, mlp_dim=spot_dim, dropout=0.) for _ in
-              range(head_layers)])
-
-        self.image_projection = ProjectionHead(embedding_dim=image_dim, projection_dim=projection_dim)
-        self.spot_projection = ProjectionHead(embedding_dim=spot_dim, projection_dim=projection_dim)
-
-        self.temperature = temperature
-
-    def forward(self, batch):
-        image_features = self.image_encoder(batch["image"])
-        spot_feature = batch["expression"]
-        image_embeddings = self.image_projection(image_features)
-
-        x = batch["position"][:, 0].long()
-        y = batch["position"][:, 1].long()
-        centers_x = self.x_embed(x)
-        centers_y = self.y_embed(y)
-
-        spot_features = spot_feature + centers_x + centers_y
-        spot_features = spot_features.unsqueeze(dim=0)
-
-        spot_embeddings = self.spot_encoder(spot_features)
-        spot_embeddings = self.spot_projection(spot_embeddings)
-        spot_embeddings = spot_embeddings.squeeze(dim=0)
-
-        cos_smi = (spot_embeddings @ image_embeddings.T) / self.temperature
-        label = torch.eye(cos_smi.shape[0], cos_smi.shape[1]).cuda()
-        spots_loss = F.cross_entropy(cos_smi, label)
-        images_loss = F.cross_entropy(cos_smi.T, label.T)
-        # loss = (images_loss + spots_loss) / 2.0
-        loss = images_loss + 3 * spots_loss
-        return loss.mean()
 
 class STco(nn.Module):
-    def __init__(self, temperature, image_embedding, spot_embedding, projection_dim, dropout=0.):
+    def __init__(self, temperature, image_embedding, spot_embedding, projection_dim, dropout=0., lamda=0.5):
         super().__init__()
         self.x_embed = nn.Embedding(128, spot_embedding)
         self.y_embed = nn.Embedding(128, spot_embedding)
@@ -274,5 +160,5 @@ class STco(nn.Module):
         targets = torch.eye(logits.shape[0], logits.shape[1]).cuda()
         spots_loss = F.cross_entropy(logits, targets, reduction='none')
         images_loss = F.cross_entropy(logits.T, targets.T, reduction='none')
-        loss = (images_loss + spots_loss) / 2.0
-        return loss.mean()
+        loss = lamda * images_loss + (1-lamda) * spots_loss
+        return loss
